e49dad125eaa4a7c1af65c9cae0da6dcda9b7a55
TTigger <sg0421051@gmail.com>
2022-12-10 02:43:17 +0800

    add comment

--- backend/src/main/java/com/pasdaven/backend/BackendApplication.java
+++ backend/src/main/java/com/pasdaven/backend/BackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
+//	dog
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
