1c292e7345005145ad181f44195e9574db78e7c7
IMGMANITSME <wayne0000432@gmail.com>
2022-12-10 02:46:37 +0800

    add comment

--- backend/src/main/java/com/pasdaven/backend/BackendApplication.java
+++ backend/src/main/java/com/pasdaven/backend/BackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
+	//41
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
