a717747d69e783ff47f279abe11e03d2ace5f9b4
IMGMANITSME <101241626+IMGMANITSME@users.noreply.github.com>
2022-12-13 00:34:00 +0800

    [Gcard-71] add getAllBoard() (#18)

--- backend/src/main/java/com/pasdaven/backend/controller/BoardController.java
+++ backend/src/main/java/com/pasdaven/backend/controller/BoardController.java
@@ -2,12 +2,11 @@ package com.pasdaven.backend.controller;
 
 import com.pasdaven.backend.model.BoardEntity;
 import com.pasdaven.backend.service.BoardService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.pasdaven.backend.service.BoardService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/board")
@@ -23,5 +22,11 @@ public class BoardController {
     //create board
     BoardEntity createBoard(@RequestBody BoardEntity boardEntity) {
         return boardService.saveBoard(boardEntity);
+    }
+
+    @GetMapping("/")
+    //get all boards
+    List<BoardEntity> getAllBoards() {
+        return boardService.getAllBoards();
     }
 }
--- backend/src/main/java/com/pasdaven/backend/service/BoardService.java
+++ backend/src/main/java/com/pasdaven/backend/service/BoardService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class BoardService {
@@ -18,5 +20,9 @@ public class BoardService {
 
     public BoardEntity saveBoard(BoardEntity boardEntity) {
         return boardRepo.save(boardEntity);
+    }
+
+    public List<BoardEntity> getAllBoards() {
+        return boardRepo.findAll();
     }
 }
