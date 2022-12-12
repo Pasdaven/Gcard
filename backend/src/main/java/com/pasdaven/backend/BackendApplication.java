1a76141f3cc23b444a4a55a7cd32357645945af1
IMGMANITSME <wayne0000432@gmail.com>
2022-12-12 23:41:53 +0800

    [Gcard-68] add createBoard()

--- backend/src/main/java/com/pasdaven/backend/BackendApplication.java
+++ backend/src/main/java/com/pasdaven/backend/BackendApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
-	// david bao
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
