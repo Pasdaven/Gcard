e3273ce2502bd8e3079769b13e3ed5ac69a00e43
David Ho <davidho0403@gmail.com>
2022-12-14 02:50:00 +0800

    fix spell wrong

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import SidebarRaw from "./SidebarRaw"
+import SidebarRow from "./SidebarRow"
 import { GlobeAsiaAustraliaIcon, RectangleStackIcon } from "@heroicons/react/24/solid"
 
 function Sidebar({ current }) {
@@ -7,9 +7,9 @@ function Sidebar({ current }) {
     <div className="flex flex-col items-start pt-12 pl-12">
       <img src="/image/icon.png" alt="icon" className="h-16 mb-16 ml-6" />
       <div className="option-group space-y-8">
-        <SidebarRaw optionName="探索" Icon={GlobeAsiaAustraliaIcon} url="/" current={current} />
-        <SidebarRaw optionName="所有看板" Icon={RectangleStackIcon} url="/allBoard" current={current} />
-        <SidebarRaw optionName="追蹤中" Icon={GlobeAsiaAustraliaIcon} url="/following" current={current} />
+        <SidebarRow optionName="探索" Icon={GlobeAsiaAustraliaIcon} url="/" current={current} />
+        <SidebarRow optionName="所有看板" Icon={RectangleStackIcon} url="/allBoard" current={current} />
+        <SidebarRow optionName="追蹤中" Icon={GlobeAsiaAustraliaIcon} url="/following" current={current} />
       </div>
     </div>
   )
--- frontend/src/components/sidebar/SidebarRow.jsx
+++ frontend/src/components/sidebar/SidebarRow.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-function SidebarRaw({ Icon, optionName, current, url }) {
+function SidebarRow({ Icon, optionName, current, url }) {
   return current === optionName ? (
     <div className="text-white flex icon-center h-12 space-x-6 bg-main items-center rounded-3xl px-5 py-3 w-fit">
       <Icon className="h-8 w-8" />
@@ -18,4 +18,4 @@ function SidebarRaw({ Icon, optionName, current, url }) {
   )
 }
 
-export default SidebarRaw
+export default SidebarRow
