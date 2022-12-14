12cc8cf4ccc53d68fe38216e4f82e4c429a7c0ff
ridemountainpig <lin0905580802@gmail.com>
2022-12-15 00:03:45 +0800

    feat: add follow user method

---
+++ backend/src/main/java/com/pasdaven/backend/controller/FollowUserController.java
@@ -0,0 +1,40 @@
+package com.pasdaven.backend.controller;
+
+import com.pasdaven.backend.model.FollowUserEntity;
+import com.pasdaven.backend.model.UserEntity;
+import com.pasdaven.backend.service.FollowUserService;
+import com.pasdaven.backend.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/followUsers")
+public class FollowUserController {
+    final FollowUserService followUserService;
+    final UserService userService;
+
+    public FollowUserController(FollowUserService followUserService, UserService userService) {
+        this.followUserService = followUserService;
+        this.userService = userService;
+    }
+
+    @PostMapping("/{userId}/{followId}")
+    public ResponseEntity<FollowUserEntity> addFollowUser(@PathVariable Integer userId, @PathVariable Integer followId) {
+        UserEntity user = userService.getUserById(userId);
+        UserEntity followUser = userService.getUserById(followId);
+
+        FollowUserEntity.FollowUserId followUserId = new FollowUserEntity.FollowUserId();
+        followUserId.setFollowerId(userId);
+        followUserId.setFollowedId(followId);
+
+        FollowUserEntity followUserEntity = new FollowUserEntity();
+        followUserEntity.setId(followUserId);
+        followUserEntity.setFollower(user);
+        followUserEntity.setFollowed(followUser);
+        followUserService.saveFollowUser(followUserEntity);
+        return new ResponseEntity<>(followUserEntity, HttpStatus.CREATED);
+    }
+}
