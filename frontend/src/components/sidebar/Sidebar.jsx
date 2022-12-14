0e13a63b5f0a9d70b835e9026b3feb1e9cf2105a
Ho Yi Wei <75478661+davidho0403@users.noreply.github.com>
2022-12-14 14:30:30 +0800

    style: formatting

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,15 +1,33 @@
-import React from "react"
-import SidebarRow from "./SidebarRow"
-import { GlobeAsiaAustraliaIcon, RectangleStackIcon } from "@heroicons/react/24/solid"
+import React from 'react'
+import SidebarRow from './SidebarRow'
+import {
+  GlobeAsiaAustraliaIcon,
+  RectangleStackIcon,
+} from '@heroicons/react/24/solid'
 
 function Sidebar({ current }) {
   return (
     <div className="flex flex-col items-start pt-12 pl-12">
       <img src="/image/icon.png" alt="icon" className="h-16 mb-16 ml-6" />
       <div className="option-group space-y-8">
-        <SidebarRow optionName="探索" Icon={GlobeAsiaAustraliaIcon} url="/" current={current} />
-        <SidebarRow optionName="所有看板" Icon={RectangleStackIcon} url="/allBoard" current={current} />
-        <SidebarRow optionName="追蹤中" Icon={GlobeAsiaAustraliaIcon} url="/following" current={current} />
+        <SidebarRow
+          optionName="探索"
+          Icon={GlobeAsiaAustraliaIcon}
+          url="/"
+          current={current}
+        />
+        <SidebarRow
+          optionName="所有看板"
+          Icon={RectangleStackIcon}
+          url="/allBoard"
+          current={current}
+        />
+        <SidebarRow
+          optionName="追蹤中"
+          Icon={GlobeAsiaAustraliaIcon}
+          url="/following"
+          current={current}
+        />
       </div>
     </div>
   )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        bg: "#1F232D",
-        card: "#252A36",
-        box: "#323847",
-        main: "#5A69E6",
-        tGray: "#B8B8B8",
+        bg: '#1F232D',
+        card: '#252A36',
+        box: '#323847',
+        main: '#5A69E6',
+        tGray: '#B8B8B8',
       },
     },
   },
