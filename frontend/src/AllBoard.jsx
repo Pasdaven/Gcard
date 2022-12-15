8e422cbd662d6d860aaca5608da73ec99e9cfdb7
TTigger <sg0421051@gmail.com>
2022-12-15 20:17:21 +0800

    add AllBoard.jsx AllBoardItem.jsx and tailwind plugin(tailwind-scrollbar-hide)

--- frontend/src/AllBoard.jsx
+++ frontend/src/AllBoard.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
 
 function AllBoard() {
-  return <div>AllBoard</div>
+  const [data, setData] = useState([])
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/api/board/')
+      .then(function (response) {
+        setData(response.data)
+      })
+      .catch((error) => console.log(error))
+  }, [data])
+
+  return (
+    <>
+      <div
+        className="grid grid-cols-2 xl:grid-cols-3 
+    overflow-y-auto max-h-screen scrollbar-hide gap-12 p-12 justify-items-center"
+      >
+        {data.map((d) => (
+          <AllBoardItem
+            boardId={d.boardId}
+            boardName={d.boardName}
+            description={d.description}
+            iconUrl={d.iconUrl}
+          />
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default AllBoard
