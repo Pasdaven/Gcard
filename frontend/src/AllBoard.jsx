18c8a1abb38e7461b2b1e01143e8c3fae14beac6
TTigger <102512862+TTigger@users.noreply.github.com>
2022-12-15 20:38:24 +0800

    add AllBoard.jsx AllBoardItem.jsx and tailwind plugin(tailwind-scrollbar-hide) (#39)

--- frontend/src/AllBoard.jsx
+++ frontend/src/AllBoard.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import AllBoardItem from './components/AllBoard/AllBoardItem'
 
 function AllBoard() {
-  return <div>AllBoard</div>
+  const [data, setData] = useState([])
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/api/board/')
+      .then(function (response) {
+        setData(response.data)
+      })
+      .catch((error) => console.log(error))
+  }, [data])
+
+  return (
+    <>
+      <div
+        className="grid grid-cols-2 xl:grid-cols-3 
+    overflow-y-auto max-h-screen scrollbar-hide gap-12 p-12 justify-items-center"
+      >
+        {data.map((d) => (
+          <AllBoardItem
+            key={d.boardId}
+            boardId={d.boardId}
+            boardName={d.boardName}
+            description={d.description}
+            iconUrl={d.iconUrl}
+          />
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default AllBoard
---
+++ frontend/src/components/AllBoard/AllBoardItem.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import PropTypes from 'prop-types'
+
+async function getBoard() {
+  axios
+    .get('http://localhost:8080/api/board/')
+    .then(function (response) {
+      console.log(response.data)
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+}
+
+function AllBoardItem({ boardId, boardName, iconUrl }) {
+  return (
+    <>
+      <Link
+        onClick={getBoard}
+        to={`/board/${boardId}`}
+        className="text-white h-52 w-52"
+      >
+        <div className="bg-box w-full h-full rounded-lg flex flex-col items-center justify-center hover:bg-gray-600">
+          <img src={`${iconUrl}`} className="h-16 w-16 m-5"></img>
+          <p className="text-xl">{boardName}</p>
+        </div>
+      </Link>
+    </>
+  )
+}
+
+AllBoardItem.propTypes = {
+  boardId: PropTypes.nubmer.isRequired,
+  boardName: PropTypes.string.isRequired,
+  iconUrl: PropTypes.string.isRequired,
+}
+
+export default AllBoardItem
