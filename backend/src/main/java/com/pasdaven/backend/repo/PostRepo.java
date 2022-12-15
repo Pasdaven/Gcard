b87bbc08ebfa9c8904b4ede4180de76e0a73c2d3
pascal910107 <95022717+pascal910107@users.noreply.github.com>
2022-12-16 01:31:29 +0800

    Get all posts by userId
    Get all posts by userId

--- backend/src/main/java/com/pasdaven/backend/repo/PostRepo.java
+++ backend/src/main/java/com/pasdaven/backend/repo/PostRepo.java
@@ -1,9 +1,13 @@
 package com.pasdaven.backend.repo;
 
 import com.pasdaven.backend.model.PostEntity;
+import com.pasdaven.backend.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepo extends JpaRepository<PostEntity, Integer> {
+    List<PostEntity> findAllByUser(UserEntity user);
 }
