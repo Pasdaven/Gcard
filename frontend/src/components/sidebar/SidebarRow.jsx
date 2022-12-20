1167bc7b7dd33c5e05f29eed44446092b70314d7
David Ho <davidho0403@gmail.com>
2022-12-21 00:45:00 +0800

    fix: change `<Link>` to `<a>` to do a force reload

--- frontend/src/components/sidebar/SidebarRow.jsx
+++ frontend/src/components/sidebar/SidebarRow.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 function SidebarRow({ Icon, optionName, current, url }) {
@@ -9,13 +8,13 @@ function SidebarRow({ Icon, optionName, current, url }) {
       <p className="tracking-[.4rem] text-xl">{optionName}</p>
     </div>
   ) : (
-    <Link
-      to={url}
+    <a
+      href={url}
       className="text-white flex icon-center h-12 space-x-6 items-center rounded-3xl px-5 py-3 w-fit hover:bg-box duration-200 cursor-pointer"
     >
       <Icon className="h-8 w-8" />
       <p className="tracking-[.4rem] text-xl">{optionName}</p>
-    </Link>
+    </a>
   )
 }
 
