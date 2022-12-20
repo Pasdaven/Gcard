87513035d03321cc2c1e6365d7331e41e3a657c9
TTigger <sg0421051@gmail.com>
2022-12-20 16:28:50 +0800

    add Following.jsx FollowingItem.jsx

--- frontend/src/Following.jsx
+++ frontend/src/Following.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import FollowingItem from './components/Following/FollowingItem'
 
 function Following() {
-  return <div>Following</div>
+  return (
+    <>
+      <div className="pt-16 pr-8 space-y-8 w-full">
+        <FollowingItem />
+        <FollowingItem />
+        <FollowingItem />
+      </div>
+    </>
+  )
 }
 
 export default Following
