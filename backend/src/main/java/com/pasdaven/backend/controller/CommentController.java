e4bfc81436e77300bce96494ca69dc8bb30a9dd9
ridemountainpig <lin0905580802@gmail.com>
2022-12-20 21:59:08 +0800

    feat: add get comment by post id

--- backend/src/main/java/com/pasdaven/backend/controller/CommentController.java
+++ backend/src/main/java/com/pasdaven/backend/controller/CommentController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/comments")
@@ -46,5 +47,15 @@ public class CommentController {
         comment.setPost(post);
         CommentEntity newComment = commentService.saveComment(comment);
         return new ResponseEntity<>(newComment, HttpStatus.OK);
+    }
+
+    @GetMapping("/post/{postId}")
+    public ResponseEntity<List<CommentEntity>> getCommentsByPostId(@PathVariable int postId) {
+        PostEntity post = postService.getPostById(postId);
+        List<CommentEntity> comments = commentService.getCommentsByPost(post);
+        for (CommentEntity comment : comments) {
+            comment.getUser().setUserAccount(null);
+        }
+        return new ResponseEntity<>(comments, HttpStatus.OK);
     }
 }
--- backend/src/main/java/com/pasdaven/backend/service/CommentService.java
+++ backend/src/main/java/com/pasdaven/backend/service/CommentService.java
@@ -1,10 +1,13 @@
 package com.pasdaven.backend.service;
 
 import com.pasdaven.backend.model.CommentEntity;
+import com.pasdaven.backend.model.PostEntity;
 import com.pasdaven.backend.repo.CommentRepo;
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -18,5 +21,9 @@ public class CommentService {
 
     public CommentEntity saveComment(CommentEntity comment) {
         return commentRepo.save(comment);
+    }
+
+    public List<CommentEntity> getCommentsByPost(PostEntity post) {
+        return commentRepo.findAllByPost(post);
     }
 }
