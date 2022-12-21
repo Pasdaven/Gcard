594cd827d9c6a1719805d0717ae91b0f3db65503
Yen Cheng <92412722+ridemountainpig@users.noreply.github.com>
2022-12-21 08:35:32 +0800

    [Gcard 105] add getCommentsByPostId method (#80)

--- backend/src/main/java/com/pasdaven/backend/repo/CommentRepo.java
+++ backend/src/main/java/com/pasdaven/backend/repo/CommentRepo.java
@@ -1,9 +1,13 @@
 package com.pasdaven.backend.repo;
 
 import com.pasdaven.backend.model.CommentEntity;
+import com.pasdaven.backend.model.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepo extends JpaRepository<CommentEntity, Integer> {
+    List<CommentEntity> findAllByPost(PostEntity post);
 }
