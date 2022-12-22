c0aa4296eb7e5cab79eb5cc80d344436741732ef
David Ho <davidho0403@gmail.com>
2022-12-23 01:06:18 +0800

    feat: fetch data while init page

--- frontend/src/pages/Explore.jsx
+++ frontend/src/pages/Explore.jsx
@@ -1,36 +1,49 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import SearchInput from '../components/search/SearchInput'
 import PostPreview from '../components/post/PostPreview'
 
 function Explore() {
-  return (
+  const [postData, setPostData] = useState()
+  const [loading, setLoading] = useState(true)
+
+  const fetchPostData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/api/post/latest')
+      setPostData(response.data)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchPostData()
+  }, [])
+
+  return loading ? (
+    <></>
+  ) : (
     <>
       <div className="m-12">
         <SearchInput />
       </div>
       <div className="mx-12 space-y-12">
-        <PostPreview
-          title="ETH 2.0 the merge is coming"
-          contentPreview="ETH 2.0 the merge is coming, let's go to Bybit and hold eth!"
-          userIcon="https://avatars.githubusercontent.com/u/75478661?v=4"
-          userName="David"
-          boardName="ETH"
-          boardIcon="https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Ethereum-ETH-icon.png"
-          userId={4}
-          boardId={3}
-          postId={5}
-        />
-        <PostPreview
-          title="ETH 2.0 the merge is coming"
-          contentPreview="ETH 2.0 the merge is coming, let's go to Bybit and hold eth!"
-          userIcon="https://avatars.githubusercontent.com/u/75478661?v=4"
-          userName="David"
-          boardName="ETH"
-          boardIcon="https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Ethereum-ETH-icon.png"
-          userId={4}
-          boardId={3}
-          postId={5}
-        />
+        {postData.map((data) => (
+          <PostPreview
+            key={data.postId}
+            title={data.title}
+            contentPreview={data.content}
+            userIcon={data.user.imgUrl}
+            userName={data.user.userName}
+            boardName={data.board.boardName}
+            boardIcon={data.board.iconUrl}
+            userId={data.user.userId}
+            boardId={data.board.boardId}
+            postId={data.postId}
+          />
+        ))}
       </div>
     </>
   )
