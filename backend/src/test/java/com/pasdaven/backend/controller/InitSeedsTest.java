3777bbe31dead41722ff09b1a06372aa3ceed482
pascal910107 <95022717+pascal910107@users.noreply.github.com>
2022-12-23 07:20:51 +0800

    Create the parent class of the test class
    Create the parent class of the test class for initializing seeds and variables

---
+++ backend/src/test/java/com/pasdaven/backend/controller/InitSeedsTest.java
@@ -0,0 +1,100 @@
+package com.pasdaven.backend.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pasdaven.backend.model.*;
+import com.pasdaven.backend.service.*;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class InitSeedsTest {
+    @Autowired
+    protected ApplicationBoardService applicationBoardService;
+    @Autowired
+    protected CommentService commentService;
+    @Autowired
+    protected FollowBoardService followBoardService;
+    @Autowired
+    protected FollowUserService followUserService;
+    @Autowired
+    protected LikePostService likePostService;
+    @Autowired
+    protected UserAccountService userAccountService;
+    @Autowired
+    protected PostService postService;
+    @Autowired
+    protected UserService userService;
+    @Autowired
+    protected BoardService boardService;
+
+    @Autowired
+    protected JWTService jwtService;
+    @Resource
+    protected MockMvc mockMvc;
+
+    protected String token_one = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoidGVzdDFAZ21haWwuY29tIn0.CuLAp33Eaz7EOUSKPWlTZuZQ9NgR2TIfheIY0WrjVRI";
+    protected String token_two = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjIsImVtYWlsIjoidGVzdDJAZ21haWwuY29tIn0.Vx6j7eyF8qM7F9tfmbga5GV5GCtV9QugoS760UoliWY";
+    protected String token_four = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjQsImVtYWlsIjoidGVzdDRAZ21haWwuY29tIn0.63zLBqCYoPR0Qg2Xs4SKrrFmvmnZRMmX_ey6g54yid4";
+
+
+    @BeforeAll
+    void setUp() throws Exception {
+        FileSystemService fileSystemService = new FileSystemService();
+        ObjectMapper mapper = new ObjectMapper();
+        // 讀取 users.json 檔
+        List<UserEntity> users = mapper.readValue(fileSystemService.readFile("src/test/resources/seeds/users.json"), mapper.getTypeFactory().constructCollectionType(List.class, UserEntity.class));
+        //讀取 boards.json 檔
+        List<BoardEntity> boards = mapper.readValue(fileSystemService.readFile("src/test/resources/seeds/boards.json"), mapper.getTypeFactory().constructCollectionType(List.class, BoardEntity.class));
+        //讀取 posts.json 檔
+        List<PostEntity> posts = mapper.readValue(fileSystemService.readFile("src/test/resources/seeds/posts.json"), mapper.getTypeFactory().constructCollectionType(List.class, PostEntity.class));
+        //讀取 comments.json 檔
+        List<CommentEntity> comments = mapper.readValue(fileSystemService.readFile("src/test/resources/seeds/comments.json"), mapper.getTypeFactory().constructCollectionType(List.class, CommentEntity.class));
+        //讀取 application_boards.json 檔
+        List<ApplicationBoardEntity> applicationBoards = mapper.readValue(fileSystemService.readFile("src/test/resources/seeds/application_boards.json"), mapper.getTypeFactory().constructCollectionType(List.class, ApplicationBoardEntity.class));
+        // 清空資料庫
+        userService.truncateAllTables();
+        postService.deleteAllPosts();
+        boardService.deleteAllBoards();
+        userService.deleteAllUsers();
+        userService.redistributeAutoIncrementNumbers();
+        // 將 users 存入資料庫
+        for (UserEntity user : users) {
+            UserAccountEntity newUserAccount = userAccountService.saveUserAccount(user.getUserAccount());
+            UserEntity newUser = userService.saveUser(user);
+            newUserAccount.setUser(newUser);
+            userAccountService.saveUserAccount(newUserAccount);
+        }
+        // 將 boards 存入資料庫
+        for (BoardEntity board : boards) {
+            boardService.saveBoard(board);
+        }
+        // 將 posts 存入資料庫
+        for (PostEntity post : posts) {
+            postService.savePost(post);
+        }
+        // 將 comments 存入資料庫
+        for (CommentEntity comment : comments) {
+            commentService.saveComment(comment);
+        }
+        // 將 application_boards 存入資料庫
+        for (ApplicationBoardEntity applicationBoard : applicationBoards) {
+            applicationBoardService.saveApplicationBoard(applicationBoard);
+        }
+    }
+}
