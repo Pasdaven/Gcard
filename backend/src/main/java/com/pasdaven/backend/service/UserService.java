14ef36d6c0b0086369668e5e1b2bba77b5d0b1b9
pascal910107 <95022717+pascal910107@users.noreply.github.com>
2022-12-23 13:19:45 +0800

    Controller Test and other fixes (#128)
    Co-authored-by: David Ho <davidho0403@gmail.com>

--- backend/src/main/java/com/pasdaven/backend/service/UserService.java
+++ backend/src/main/java/com/pasdaven/backend/service/UserService.java
@@ -2,6 +2,9 @@ package com.pasdaven.backend.service;
 
 import com.pasdaven.backend.model.UserEntity;
 import com.pasdaven.backend.repo.UserRepo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
@@ -13,6 +16,9 @@ import java.util.List;
 public class UserService {
 
     private final UserRepo userRepo;
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public UserService(UserRepo userRepo) {
         this.userRepo = userRepo;
@@ -33,5 +39,52 @@ public class UserService {
 
     public List<UserEntity> searchUserByName(String keyword) {
         return userRepo.findByUserNameContaining(keyword);
+    }
+
+    public void deleteAllUsers() {
+        userRepo.deleteAll();
+    }
+
+    public void truncateAllTables() {
+        String sql = "TRUNCATE TABLE `application_board`";
+        Query query = entityManager.createNativeQuery(sql);
+        query.executeUpdate();
+        sql = "TRUNCATE TABLE `users_account`";
+        query = entityManager.createNativeQuery(sql);
+        query.executeUpdate();
+        sql = "TRUNCATE TABLE `comment`";
+        query = entityManager.createNativeQuery(sql);
+        query.executeUpdate();
+        sql = "TRUNCATE TABLE `follow_board`";
+        query = entityManager.createNativeQuery(sql);
+        query.executeUpdate();
+        sql = "TRUNCATE TABLE `follow_user`";
+        query = entityManager.createNativeQuery(sql);
+        query.executeUpdate();
+        sql = "TRUNCATE TABLE `like_post`";
+        query = entityManager.createNativeQuery(sql);
+        query.executeUpdate();
+//        sql = "SET FOREIGN_KEY_CHECKS=0;\n" +
+//                "TRUNCATE TABLE `post`;\n" +
+//                "SET FOREIGN_KEY_CHECKS=1;\n";
+//        query = entityManager.createNativeQuery(sql);
+//        query.executeUpdate();
+//        sql = "TRUNCATE TABLE `board`";
+//        query = entityManager.createNativeQuery(sql);
+//        query.executeUpdate();
+//        sql = "TRUNCATE TABLE `users`";
+//        query = entityManager.createNativeQuery(sql);
+//        query.executeUpdate();
+    }
+    public void redistributeAutoIncrementNumbers() {
+        String sql = "ALTER TABLE `post` AUTO_INCREMENT = 1";
+        Query query = entityManager.createNativeQuery(sql);
+        query.executeUpdate();
+        sql = "ALTER TABLE `board` AUTO_INCREMENT = 1";
+        query = entityManager.createNativeQuery(sql);
+        query.executeUpdate();
+        sql = "ALTER TABLE `users` AUTO_INCREMENT = 1";
+        query = entityManager.createNativeQuery(sql);
+        query.executeUpdate();
     }
 }
