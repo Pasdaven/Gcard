388d987d2f15bec009087cc6bdcfa12bfb1774bd
TTigger <102512862+TTigger@users.noreply.github.com>
2022-12-24 04:13:34 +0800

    add Following.jsx and FollowingItem.jsx (#148)

--- frontend/src/components/follow/FollowingItem.jsx
+++ frontend/src/components/follow/FollowingItem.jsx
@@ -1,31 +1,38 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function FollowingItem() {
+function FollowingItem({ userId, userIcon, name, followingCount, fansCount }) {
   return (
     <>
-      <div className="flex justify-between rounded-lg bg-box text-white py-8 px-12 text-xl tracking-widest">
-        <div className="flex items-center">
-          <img
-            src="https://t3.ftcdn.net/jpg/02/17/04/38/360_F_217043893_C16NUEZ1x9xL6W3znWZNVn15TrIp4lc7.jpg"
-            className="h-10 w-10 mr-4 rounded-full"
-          ></img>
-          <p>Dale Mitchelle</p>
-        </div>
-
-        <div className="flex">
-          <div className="flex flex-col items-center justify-center px-12">
-            <p>135</p>
-            <p className="text-tGray text-base">追蹤中</p>
+      <a href={`/user/${userId}`}>
+        <div className="flex justify-between rounded-lg bg-box text-white py-8 px-12 text-xl tracking-widest mb-8">
+          <div className="flex items-center">
+            <img src={userIcon} className="h-10 w-10 mr-4 rounded-full"></img>
+            <p>{name}</p>
           </div>
 
-          <div className="flex flex-col items-center justify-center px-2">
-            <p>23</p>
-            <p className="text-tGray text-base">粉絲</p>
+          <div className="flex">
+            <div className="flex flex-col items-center justify-center px-12">
+              <p>{followingCount}</p>
+              <p className="text-tGray text-base">追蹤中</p>
+            </div>
+
+            <div className="flex flex-col items-center justify-center px-2">
+              <p>{fansCount}</p>
+              <p className="text-tGray text-base">粉絲</p>
+            </div>
           </div>
         </div>
-      </div>
+      </a>
     </>
   )
+}
+FollowingItem.propTypes = {
+  userId: PropTypes.number.isRequired,
+  userIcon: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  followingCount: PropTypes.number.isRequired,
+  fansCount: PropTypes.number.isRequired,
 }
 
 export default FollowingItem
