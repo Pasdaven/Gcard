d0ea99a95bc70079746492dcff088c434191338b
Ho Yi Wei <75478661+davidho0403@users.noreply.github.com>
2022-12-24 15:29:39 +0800

    feat: filter of search feature (#163)

--- frontend/src/pages/SearchResult.jsx
+++ frontend/src/pages/SearchResult.jsx
@@ -1,59 +1,40 @@
-import axios from 'axios'
 import React from 'react'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
-import PostPreview from '../components/post/PostPreview'
 import SearchInput from '../components/search/SearchInput'
+import SearchFilter from '../components/search/SearchFilter'
+import AllSearchResult from '../components/search/AllSearchResult'
 
 function SearchResult() {
   const { keyword } = useParams()
-  const [postData, setPostData] = useState()
+  const [searchTarget, setSearchTarget] = useState('post')
   const [loading, setLoading] = useState(true)
 
-  const fetchSearchData = async () => {
-    try {
-      const response = await axios.get(
-        `http://localhost:8080/api/post/?keyword=${keyword}`
-      )
-      setPostData(response.data)
-      setLoading(false)
-    } catch (error) {
-      console.log(error)
-    }
+  const updateSearchTarget = (target) => {
+    setLoading(true)
+    setSearchTarget(target)
   }
 
-  useEffect(() => {
-    fetchSearchData()
-  }, [])
-  return loading ? (
-    <></>
-  ) : (
-    <>
-      <div className="m-12">
+  const updateLoading = (loading) => {
+    setLoading(loading)
+  }
+
+  return (
+    <div className="mx-12">
+      <div className="my-12">
         <SearchInput />
       </div>
-      <div className="mx-12 my-10">
-        <p className="tracking-[.4rem] text-tGray text-sm">
-          {keyword} {postData.length ? '的搜尋結果如下' : '查無任何結果'}
-        </p>
-      </div>
-      <div className="mx-12 space-y-12">
-        {postData.map((data) => (
-          <PostPreview
-            key={data.postId}
-            title={data.title}
-            contentPreview={data.content}
-            userIcon={data.user.imgUrl}
-            userName={data.user.userName}
-            boardName={data.board.boardName}
-            boardIcon={data.board.iconUrl}
-            userId={data.user.userId}
-            boardId={data.board.boardId}
-            postId={data.postId}
-          />
-        ))}
-      </div>
-    </>
+      <SearchFilter
+        searchTarget={searchTarget}
+        setSearchTarget={updateSearchTarget}
+      />
+      <AllSearchResult
+        keyword={keyword}
+        searchTarget={searchTarget}
+        loading={loading}
+        updateLoading={updateLoading}
+      />
+    </div>
   )
 }
 
