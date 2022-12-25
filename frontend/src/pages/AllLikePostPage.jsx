bd0b2ecdbfd898797a10e4d26377d375823483c5
TTigger <102512862+TTigger@users.noreply.github.com>
2022-12-26 01:55:12 +0800

    All like post page (#174)
    Co-authored-by: David Ho <davidho0403@gmail.com>

---
+++ frontend/src/pages/AllLikePostPage.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import SearchInput from '../components/search/SearchInput'
+import { RectangleStackIcon } from '@heroicons/react/24/outline'
+import PostPreview from '../components/post/PostPreview'
+
+function AllLikePostPage() {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const token = localStorage.getItem('jwt_token')
+
+  const fetchData = async () => {
+    event.preventDefault()
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/api/likePosts/token`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      console.log(response.data)
+      setData(response.data)
+    } catch (error) {
+      console.error(error)
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return loading ? (
+    <></>
+  ) : (
+    <>
+      <div className="m-12">
+        <SearchInput />
+      </div>
+      <div className="text-white m-12 text-2xl tracking-[.4rem] flex space-x-2 items-center">
+        <RectangleStackIcon className="h-7 w-7" />
+        <h1>按過的讚</h1>
+      </div>
+      <div className="m-12 space-y-8">
+        {data.map((d) => (
+          <PostPreview
+            key={d.post.postId}
+            postId={d.post.postId}
+            title={d.post.title}
+            contentPreview={d.post.content}
+            userIcon={d.post.user.imgUrl}
+            userName={d.post.user.userName}
+            boardName={d.post.board.boardName}
+            boardIcon={d.post.board.iconUrl}
+            boardId={d.post.board.boardId}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default AllLikePostPage
