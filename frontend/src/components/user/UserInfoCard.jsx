26e3321ae15128135afc653faaa35ae31afc4081
David Ho <davidho0403@gmail.com>
2022-12-26 02:37:25 +0800

    feat: follow user feature

--- frontend/src/components/user/UserInfoCard.jsx
+++ frontend/src/components/user/UserInfoCard.jsx
@@ -1,11 +1,76 @@
 import React from 'react'
 import { HeartIcon } from '@heroicons/react/24/solid'
 import PropTypes from 'prop-types'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
-function userInfoCard({ userName, userIcon, fansCount, followingCount }) {
-  return (
+function userInfoCard({
+  userId,
+  userName,
+  userIcon,
+  fansCount,
+  followingCount,
+}) {
+  const [loading, setLoading] = useState(true)
+  const [isFollow, setIsFollow] = useState(false)
+
+  const fetchIsFollowUser = async () => {
+    try {
+      const res = await axios.get(
+        `http://localhost:8080/api/followUsers/check/${userId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('jwt_token')}`,
+          },
+        }
+      )
+      setIsFollow(res.data)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const updateFollowUser = async () => {
+    if (isFollow) {
+      // delete follow
+      try {
+        await axios.delete(`http://localhost:8080/api/followUsers/${userId}`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('jwt_token')}`,
+          },
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    } else {
+      try {
+        await axios.post(
+          `http://localhost:8080/api/followUsers/${userId}`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('jwt_token')}`,
+            },
+          }
+        )
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchIsFollowUser()
+    location.reload()
+  }
+
+  useEffect(() => {
+    fetchIsFollowUser()
+  }, [])
+  return loading ? (
+    <></>
+  ) : (
     <>
-      <div className="flex w-full justify-between">
+      <a className="flex w-full justify-between" href={`/user/${userId}`}>
         {/* User Name and icon */}
         <div className="flex items-center space-x-6">
           <span
@@ -33,18 +98,32 @@ function userInfoCard({ userName, userIcon, fansCount, followingCount }) {
             <label className="text-tGray text-sm">粉絲</label>
           </div>
         </div>
-      </div>
+      </a>
       <div className="flex w-full justify-end mt-4">
-        <button className="flex text-white items-center space-x-2 px-5 py-2 border-gray-700 border-[1px] rounded-lg hover:bg-box duration-150">
-          <HeartIcon className="h-6 w-6" />
-          <span className="tracking-[.5rem]">追蹤</span>
-        </button>
+        {isFollow ? (
+          <button
+            className="flex text-white items-center space-x-2 px-5 py-2 border-gray-700 border-[1px] rounded-lg hover:bg-box duration-150"
+            onClick={() => updateFollowUser()}
+          >
+            <HeartIcon className="h-6 w-6" />
+            <span className="tracking-[.5rem]">取消追蹤</span>
+          </button>
+        ) : (
+          <button
+            className="flex text-white items-center space-x-2 px-5 py-2 border-gray-700 border-[1px] rounded-lg hover:bg-box duration-150"
+            onClick={() => updateFollowUser()}
+          >
+            <HeartIcon className="h-6 w-6" />
+            <span className="tracking-[.5rem]">追蹤</span>
+          </button>
+        )}
       </div>
     </>
   )
 }
 
 userInfoCard.propTypes = {
+  userId: PropTypes.number.isRequired,
   userName: PropTypes.string.isRequired,
   userIcon: PropTypes.string.isRequired,
   fansCount: PropTypes.number.isRequired,
